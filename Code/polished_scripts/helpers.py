fd8580a5551c39f39b544f1da8595fbb11a3556f
Lawrence Huang <lawrence_huang1@brown.edu>
2019-08-15 11:56:47 -0400

    HTSeq script working

--- Code/polished_scripts/helpers.py
+++ Code/polished_scripts/helpers.py
@@ -13,13 +13,37 @@ def _handle_directory_cleaning(dir_path):
         split_output = os.sep.join(split_output)
         return os.path.join(os.getcwd(), split_output)
     return dir_path
-        
+
+def _remove_periods_from_front(input):
+    """Removes "." character from beginning of input string"""
+    while input and input.startswith("."):
+        input = input[1:]
+    return input
+    
 def _split_all_ext(input_path):
     """Returns path with all extensions removed"""
-    ext_removed_path = input_path
+    ext_removed_path = os.path.basename(input_path)
     ext_removed = ""
-    while "." in ext_removed_path:
+    
+    while "." in _remove_periods_from_front(ext_removed_path):
         (ext_removed_path, ext_removed_part) = os.path.splitext(ext_removed_path)
         ext_removed = ext_removed_part + ext_removed
         
-    return (ext_removed_path, ext_removed)
+    return (os.path.join(os.path.dirname(input_path), ext_removed_path), ext_removed)
+
+if __name__ == "__main__":
+    """Jank testing"""
+    print(_remove_periods_from_front(None))
+    print(_remove_periods_from_front("...sdkflj"))
+    print(_remove_periods_from_front("sdkflj"))
+    
+    print(_split_all_ext("."))
+    print(_split_all_ext("./"))
+    print(_split_all_ext("./abc"))
+    print(_split_all_ext("/.abc"))
+    print(_split_all_ext("/.abc.gz"))
+    print(_split_all_ext("./abc.sd.sdf"))
+    print(_split_all_ext("./abc/def.ds"))
+    print(_split_all_ext("/abc/def.ds"))
+    print(_split_all_ext("/abc.def.ds"))
+    print(_split_all_ext("/n/data1/hms/dbmi/park/lawrence/FCD/PRJNA527057_kobow_et_al_epilepsia2019/.HTSeq/.sh/SRR8728543_1_fixedAligned_HTSeq.sh"))
